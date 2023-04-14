c6c6e458a00e10dfec99b4cda650a352c2626c47
John Ramos <jramos5883@gmail.com>
2023-04-13 18:41:20 -0700

    Finished menu page

--- pttm-website/src/routes/menu/menu.comp.tsx
+++ pttm-website/src/routes/menu/menu.comp.tsx
@@ -1,7 +1,21 @@
+import { Fragment } from "react";
+import Footer from "../../components/footer/footer.comp";
+
 import "./menu.styles.css";
 
 const Menu = () => {
-  return <div>Menu!</div>;
+  return (
+    <Fragment>
+      <div className="menu-container">
+        <img
+          className="container-fluid"
+          src="/images/pokemenu.png"
+          alt="PokeToTheMoon-Resturant"
+        />
+      </div>
+      <Footer />
+    </Fragment>
+  );
 };
 
 export default Menu;
